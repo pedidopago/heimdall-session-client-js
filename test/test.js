93952e99d5120d654fdfa8a7bb0f6b3a6855e300
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2022-03-03 13:36:26 -0300

    test example (todo tests)

--- test/test.js
+++ test/test.js
@@ -2,3 +2,8 @@
 var expect = require("chai").expect;
 var index = require("../dist/index");
 
+// describe("test something", () => {
+//   it("should return something", async () => {
+//     expect(msg.content).to.equal("hello");
+//   });
+// });
